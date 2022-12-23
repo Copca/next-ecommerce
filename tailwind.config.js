ce6377fe13d35dc886cab4c80eef345c2921e392
Ernest <dite_malacoda@hotmail.com>
2022-12-23 12:51:00 -0600

    instalacion tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		container: {
+			center: true,
+
+			padding: {
+				DEFAULT: '1rem',
+				// sm: '2rem',
+				// lg: '4rem'
+				// xl: '5rem',
+				'2xl': '6rem'
+			}
+		},
+		extend: {
+			animation: {
+				fadeIn: 'fadeIn 1s 1',
+				fadeOut: 'fadeOut 1s 1'
+			},
+			keyframes: {
+				fadeIn: {
+					'0%': { opacity: 0 },
+					'100%': { opacity: 1 }
+				},
+				fadeOut: {
+					'0%': { opacity: 1 },
+					'100%': { opacity: 0 }
+				}
+			}
+		}
+	},
+	plugins: []
+};
