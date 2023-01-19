ae0ae153e0b808ee61a7c55dc40cc7f37eb9a431
Ernest <dite_malacoda@hotmail.com>
2023-01-19 17:43:32 -0600

    use-hook-form RegisterPage

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -2,11 +2,15 @@
  * Restringimos la entrada a la página con SSR (getServerSideProps), si esta logeado
  */
 
+import { useState } from 'react';
 import { GetServerSideProps, NextPage } from 'next';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import { useForm } from 'react-hook-form';
 
 import { AiFillGithub } from 'react-icons/ai';
+
+import { validation } from '../../utils';
 
 import { AuthLayout } from '../../components/layout';
 
@@ -18,6 +22,17 @@ interface FormData {
 
 const RegisterPage: NextPage = () => {
 	const router = useRouter();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm<FormData>();
+	const [isShowError, setIsShowError] = useState(false);
+	const [errorMessage, setErrorMessage] = useState('');
+
+	const onRegisterUser = async ({ name, email, password }: FormData) => {
+		console.log({ name, email, password });
+	};
 
 	return (
 		<AuthLayout title={'Crear cuenta'}>
@@ -25,19 +40,25 @@ const RegisterPage: NextPage = () => {
 				<div className='max-w-lg mx-auto bg-white border shadow rounded-md p-8'>
 					<h1 className='text-2xl font-bold mb-8'>Crear cuenta</h1>
 
-					<form>
+					{isShowError && (
+						<div className='bg-red-500 text-white text-center text-sm rounded-full py-1.5 mb-8 w-4/5 mx-auto'>
+							Este usuario ya esta registrado
+						</div>
+					)}
+
+					<form onSubmit={handleSubmit(onRegisterUser)}>
 						<div className={`relative mb-4`}>
 							<input
 								type='text'
 								placeholder=' '
 								className='w-full border focus:border-sky-500 rounded-md outline-none py-2 px-3 peer'
-								// {...register('name', {
-								// 	required: 'Este campo es obligatorio'
-								// })}
+								{...register('name', {
+									required: 'Este campo es obligatorio'
+								})}
 							/>
 							<label className='label-float'>Nombre</label>
 							<p className='mt-1 ml-2 peer-invalid:visible text-pink-600 text-xs'>
-								{/* {errors.name && errors.name?.message} */}
+								{errors.name && errors.name?.message}
 							</p>
 						</div>
 
@@ -46,14 +67,14 @@ const RegisterPage: NextPage = () => {
 								type='email'
 								placeholder=' '
 								className='w-full border focus:border-sky-500 rounded-md outline-none py-2 px-3 peer'
-								// {...register('email', {
-								// 	required: 'Este campo es obligatorio',
-								// 	validate: validation.isEmail
-								// })}
+								{...register('email', {
+									required: 'Este campo es obligatorio',
+									validate: validation.isEmail
+								})}
 							/>
 							<label className='label-float'>Email</label>
 							<p className='mt-1 ml-2 peer-invalid:visible text-pink-600 text-xs'>
-								{/* {errors.email && errors.email?.message} */}
+								{errors.email && errors.email?.message}
 							</p>
 						</div>
 
@@ -62,17 +83,17 @@ const RegisterPage: NextPage = () => {
 								type='password'
 								placeholder=' '
 								className='w-full border focus:border-sky-500 rounded-md outline-none py-2 px-3 peer'
-								// {...register('password', {
-								// 	required: 'Este campo es obligatorio',
-								// 	minLength: {
-								// 		value: 6,
-								// 		message: 'Mínimo 6 caracteres'
-								// 	}
-								// })}
+								{...register('password', {
+									required: 'Este campo es obligatorio',
+									minLength: {
+										value: 6,
+										message: 'Mínimo 6 caracteres'
+									}
+								})}
 							/>
 							<label className='label-float'>Password</label>
 							<p className='mt-1 ml-2 peer-invalid:visible text-pink-600 text-xs'>
-								{/* {errors.password && errors.password?.message} */}
+								{errors.password && errors.password?.message}
 							</p>
 						</div>
 
@@ -102,6 +123,7 @@ const RegisterPage: NextPage = () => {
 
 						<div className='flex justify-center'>
 							<button
+								type='button'
 								className='btn text-gray-800 hover:bg-gray-100 border border-gray-500 gap-2 w-full mt-8'
 								data-mdb-ripple='true'
 								data-mdb-ripple-color='dark'
