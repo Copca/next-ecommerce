851468961decb3d3d1859a1527f559c5e6191e56
Ernest <dite_malacoda@hotmail.com>
2023-01-25 16:39:15 -0600

    AdminDashboard

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -18,39 +18,38 @@ import { AdminLayout } from '../../components/layouts';
 import { SummaryTile } from '../../components/admin';
 
 const AdminDashboardPage: NextPage = () => {
-	// const { data, error } = useSWR<IDashboardResponse>('/api/admin/dashboard', {
-	// 	refreshInterval: 30000 // 30seg
-	// });
-
-	// const [refreshIn, setRefreshIn] = useState(30);
+	const { data, error } = useSWR<IDashboardResponse>('/api/admin/dashboard', {
+		refreshInterval: 30000 // 30seg
+	});
+	const [refreshIn, setRefreshIn] = useState(30);
 
 	// Contador de 30 seg
-	// useEffect(() => {
-	// 	const interval = setInterval(() => {
-	// 		// console.log('tick');
-	// 		setRefreshIn((refreshIn) => (refreshIn > 0 ? refreshIn - 1 : 30));
-	// 	}, 1000);
+	useEffect(() => {
+		const interval = setInterval(() => {
+			// console.log('tick');
+			setRefreshIn((refreshIn) => (refreshIn > 0 ? refreshIn - 1 : 30));
+		}, 1000);
 
-	// 	// eliminamos el interval cuando se desmonta el componente
-	// 	return () => clearInterval(interval);
-	// }, []);
+		// eliminamos el interval cuando se desmonta el componente
+		return () => clearInterval(interval);
+	}, []);
 
-	// if (!error && !data) return <></>;
+	if (!error && !data) return <></>;
 
-	// if (error) {
-	// 	console.log(error);
-	// 	return <h3>Error al cargar la información</h3>;
-	// }
+	if (error) {
+		console.log(error);
+		return <h3>Error al cargar la información</h3>;
+	}
 
-	// const {
-	// 	numberOfOrders,
-	// 	paidOrders,
-	// 	notPaidOrders,
-	// 	numberOfClients,
-	// 	numberOfProducts,
-	// 	productsWithNoInventory,
-	// 	lowInventory
-	// } = data!;
+	const {
+		numberOfOrders,
+		paidOrders,
+		notPaidOrders,
+		numberOfClients,
+		numberOfProducts,
+		productsWithNoInventory,
+		lowInventory
+	} = data!;
 
 	return (
 		<AdminLayout
@@ -61,15 +60,13 @@ const AdminDashboardPage: NextPage = () => {
 			<div className='container mt-8'>
 				<div className='grid grid-cols-2 md:grid-cols-4 gap-8'>
 					<SummaryTile
-						// title={numberOfOrders}
-						title={1}
+						title={numberOfOrders}
 						subtitle='Ordenes Totales'
 						icon={<MdCreditCard className='text-4xl text-blue-600' />}
 					/>
 
 					<SummaryTile
-						// title={paidOrders}
-						title={2}
+						title={paidOrders}
 						subtitle='Ordenes Pagadas'
 						icon={
 							<MdOutlineCreditScore className='text-4xl text-green-600' />
@@ -77,8 +74,7 @@ const AdminDashboardPage: NextPage = () => {
 					/>
 
 					<SummaryTile
-						// title={notPaidOrders}
-						title={3}
+						title={notPaidOrders}
 						subtitle='Ordenes Pendientes'
 						icon={
 							<MdOutlineCreditCardOff className='text-4xl text-red-600' />
@@ -86,36 +82,32 @@ const AdminDashboardPage: NextPage = () => {
 					/>
 
 					<SummaryTile
-						// title={numberOfClients}
-						title={4}
+						title={numberOfClients}
 						subtitle='Clientes'
 						icon={<MdOutlinePeopleAlt className='text-4xl text-slate-600' />}
 					/>
 
 					<SummaryTile
-						// title={numberOfProducts}
-						title={5}
+						title={numberOfProducts}
 						subtitle='Productos'
 						icon={<GiClothes className='text-4xl text-slate-600' />}
 					/>
 
 					<SummaryTile
-						// title={productsWithNoInventory}
-						title={6}
+						title={productsWithNoInventory}
 						subtitle='Sin existencia'
 						icon={<GiClothes className='text-4xl text-red-600' />}
 					/>
 
 					<SummaryTile
-						// title={lowInventory}
-						title={7}
+						title={lowInventory}
 						subtitle='Bajo Inventario'
 						icon={<GiClothes className='text-4xl text-yellow-600' />}
 					/>
 
 					<SummaryTile
 						// title={refreshIn}
-						title={8}
+						title={30}
 						subtitle='Actualización en:'
 						icon={<MdTimer className='text-4xl text-blue-600' />}
 					/>
